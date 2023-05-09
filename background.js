f8215d4da499d0b567de693decc4bdee1a231aee
RetrogisusDEV <99091223+RetrogisusDEV@users.noreply.github.com>
2023-05-09 10:41:19 -0400

    Update background.js
    Add code

--- background.js
+++ background.js
@@ -1,1 +1,16 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
 
+  var presence = prompt("Enter your custom Rich Presence:");
+
+  if (presence !== null && presence !== "") {
+
+    chrome.tabs.executeScript({
+
+      code: `document.querySelector('.rpc-image').src='https://i.imgur.com/OgSnyJm.png'; document.querySelector('.rpc-details').textContent='${presence}';`
+
+    });
+
+  }
+
+});
+  
